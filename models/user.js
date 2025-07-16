28fba72bdf6b2a13cbe4e23c3601f5f7274a1b40
Donte <damorgan19@gmail.com>
2025-07-16 10:27:56 -0400

    create schemas

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    reqired: true,
+    minlenth: 2,
+    maxlenth: 30,
+  },
+
+  avatar: {
+    type: String,
+    reqired: [true, "The avatar field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a Valid URL",
+    },
+  },
+});
 
 module.exports = mongoose.model("user", userSchema);
