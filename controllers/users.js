e9a428087332d1aa9957a260da29207cd12031af
Donte <damorgan19@gmail.com>
2025-07-29 13:01:27 -0400

    add login to controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,14 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const { JWT_SECRET } = require("../utils/config");
 
 const {
   BAD_REQUEST,
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
+  CONFLICT_ERROR,
+  UNAUTHORIZED,
 } = require("../utils/errors");
 
 const getUsers = (req, res) => {
@@ -18,12 +23,25 @@ const getUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => res.status(200).send(user))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      return User.create({ name, avatar, email, password: hash });
+    })
+    .then((user) => {
+      const userWithoutPassword = user.toObject();
+      delete userWithoutPassword.password;
+      res.status(201).send(userWithoutPassword);
+    })
     .catch((err) => {
-      console.error(err);
+      console.error("createUser error:", err);
+      if (err.code === 11000) {
+        return res
+          .status(CONFLICT_ERROR)
+          .send({ message: "Email already exists." });
+      }
       if (err.name === "ValidationError") {
         return res.status(BAD_REQUEST).send({ message: err.message });
       }
@@ -59,4 +77,21 @@ const getUser = (req, res) => {
         .send({ message: "Internal Server Error" });
     });
 };
-module.exports = { getUsers, createUser, getUser };
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    })
+    .catch((err) => {
+      console.error("Login error:", err);
+      res.status(UNAUTHORIZED).send({ message: "Incorrect email or password" });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser, login };
