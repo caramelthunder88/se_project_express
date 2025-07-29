b10eb601e55c2eda6461a299ab8b7804c530a442
Donte <damorgan19@gmail.com>
2025-07-29 14:46:10 -0400

    fix order in app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const mainRouter = require("./routes");
-const { login, createUser } = require("./controllers/users");
-const auth = require("./middlewares/auth");
 
 const app = express();
+const auth = require("./middlewares/auth");
+const mainRouter = require("./routes");
+const { login, createUser } = require("./controllers/users");
 
 const { PORT = 3001 } = process.env;
+
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -15,10 +19,10 @@ mongoose
   })
   .catch(console.error);
 
-app.use(express.json());
-
 app.post("/signin", login);
 app.post("/signup", createUser);
+
+app.use(auth);
 
 app.use("/", mainRouter);
 
