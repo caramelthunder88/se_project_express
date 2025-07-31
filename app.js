2c85cbb3e583519bb71150ba71b39195330ebc10
Donte <damorgan19@gmail.com>
2025-07-31 14:28:13 -0400

    add mock user

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ mongoose
 app.post("/signin", login);
 app.post("/signup", createUser);
 
+if (process.env.NODE_ENV === "test") {
+  app.use((req, res, next) => {
+    req.user = { _id: "5d8b8592978f8bd833ca8133" };
+    return next();
+  });
+}
 app.use(auth);
 
 app.use("/", mainRouter);
