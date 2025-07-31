c2d3c8b593d3e283541ff00bc2fbf4f2793fbbba
Donte <damorgan19@gmail.com>
2025-07-31 15:05:15 -0400

    remove comments and test logs

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -2,10 +2,13 @@ const { INTERNAL_SERVER_ERROR } = require("../utils/errors");
 
 module.exports = (err, req, res) => {
   const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
-  res.status(statusCode).send({
-    message:
-      statusCode === INTERNAL_SERVER_ERROR
-        ? "An internal server error occurred"
-        : message,
-  });
+  res
+    .status(statusCode)
+    .type("application/json")
+    .send({
+      message:
+        statusCode === INTERNAL_SERVER_ERROR
+          ? "An internal server error occurred"
+          : message,
+    });
 };
