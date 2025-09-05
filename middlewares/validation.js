d7e3fa7444d663702d95cab503540763ca4948ae
Donte <damorgan19@gmail.com>
2025-09-05 15:04:57 -0400

    add weather to validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -14,19 +14,30 @@ const validateURL = (value, helpers) => {
 };
 
 const validateCardBody = celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
-      "string.min": 'The minimum length of the "name" field is 2',
-      "string.max": 'The maximum length of the "name" field is 30',
-      "string.empty": 'The "name" field must be filled in',
-      "any.required": 'The "name" field is required',
-    }),
-    imageUrl: Joi.string().required().custom(validateURL).messages({
-      "string.empty": 'The "imageUrl" field must be filled in',
-      "string.uri": 'the "imageUrl" field must be a valid url',
-      "any.required": 'The "imageUrl" field is required',
-    }),
-  }),
+  [Segments.BODY]: Joi.object()
+    .keys({
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+        "any.required": 'The "name" field is required',
+      }),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "imageUrl" field must be filled in',
+        "string.uri": 'the "imageUrl" field must be a valid url',
+        "any.required": 'The "imageUrl" field is required',
+      }),
+      weather: Joi.string()
+        .lowercase()
+        .valid("hot", "warm", "cold")
+        .required()
+        .messages({
+          "any.only": 'The "weather" field must be one of: hot, warm, cold',
+          "string.empty": 'The "weather" field must be filled in',
+          "any.required": 'The "weather" field is required',
+        }),
+    })
+    .unknown(false),
 });
 
 const validateSignupBody = celebrate({
